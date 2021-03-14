2e7fdb82a39bbd93d4e4fe3d152092b3f6e7a1d8
Alex Semochkin <fennyflop@gmail.com>
2021-03-14 23:49:09 +0300

    preloader

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,10 +1,12 @@
 import './MoviesCardList.css';
 import Movie from '../Movie/Movie';
+import Preloader from '../Preloader/Preloader';
 
 function MoviesCardList() {
     return (
         <section className="movies">
             <div className="movies__list">
+                {/* <Movie />
                 <Movie />
                 <Movie />
                 <Movie />
@@ -15,9 +17,9 @@ function MoviesCardList() {
                 <Movie />
                 <Movie />
                 <Movie />
-                <Movie />
-                <Movie />
+                <Movie /> */}
             </div>
+            <Preloader />
             <button className="movies__button">Ещё</button>
         </section>
     );
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -6,15 +6,15 @@ function Registration() {
         <UserForm question="Уже зарегистрированы?" name="registration" linkName="Войти" submitName="Зарегистрироваться" title="Добро пожаловать!" toPath="/signin">
             <fieldset className="registration__fieldset">
                 <label className="registration__label" htmlFor="username">Имя</label>
-                <input className="registration__input" name="username" />
+                <input className="registration__input" autocomplete="off" name="username" />
             </fieldset>
             <fieldset className="registration__fieldset">
                 <label className="registration__label" htmlFor="email">E-mail</label>
-                <input className="registration__input" type="email" name="email" />
+                <input className="registration__input" autocomplete="off" type="email" name="email" />
             </fieldset>
             <fieldset className="registration__fieldset">
                 <label className="registration__label" htmlFor="password">Пароль</label>
-                <input className="registration__input registration__input-error" type="password" name="password" />
+                <input className="registration__input registration__input-error" autocomplete="off" type="password" name="password" />
                 <span className="registration__error">Что-то пошло не так...</span>
             </fieldset>
         </UserForm>
