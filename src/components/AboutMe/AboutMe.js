e689a8ffc1b3296be33f59afc583388b08683316
Alex Semochkin <fennyflop@gmail.com>
2021-05-09 16:18:52 +0300

    info updated

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,22 +2,24 @@ import './AboutMe.css';
 import Slide from '../Slide/Slide';
 
 import myImage from '../../images/me.png';
+import me from '../../images/meanddoge.jpg';
+
 
 function AboutMe() {
     return (
         <Slide name="aboutme" title="Студент">
             <section className="aboutme__info">
                 <div className="aboutme__text">
-                    <h2 className="aboutme__name">Виталий</h2>
-                    <p className="aboutme__occupation">Фронтенд-разработчик, 30 лет</p>
-                    <p className="aboutme__biography">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                    <h2 className="aboutme__name">Александр</h2>
+                    <p className="aboutme__occupation">Разработчик, 15 лет</p>
+                    <p className="aboutme__biography">Меня зовут Александр. В своё свободное время увлекаюсь программированием. Люблю работать над масштабными проектами, видеть свой вклад в них. В 2020 году я окунулся в веб-разработку,
+                    а сейчас тренирую свои навыки программирования на C++</p>
                     <ul className="aboutme__contacts">
                         <li><a className="aboutme__contact" target="_blank" rel="noreferrer" href="https://www.facebook.com/alex.semochkin.5/">Facebook</a></li>
                         <li><a className="aboutme__contact" target="_blank" rel="noreferrer" href="https://github.com/fennyflop">Github</a></li>
                     </ul>
                 </div>
-                <img className="aboutme__image" alt="me" src={myImage} />
+                <img className="aboutme__image" alt="me" src={me} />
             </section>
         </Slide>
     );
